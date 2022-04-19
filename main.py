b38ae861ce518dad14604f06affaa74f1bca18cb
HalaAshraf <81113286+HalaAshraf@users.noreply.github.com>
2022-04-19 11:55:28 +0200

    run to get sjf

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 16 14:11:53 2022
-
-@author: east asia
-"""
-
 import prenonsjf
+
 num = int(input('Enter number of processes '))
+
+#define processes list
 proc = [[0]*3]*num
+
+# define data dictionary
 gData = {}
+
+# ask user for input
 for i in range(num):
-    id = input('Enter  process ID ')
-    print('For process '+id)
+    
+    ID = input('Enter  process ID ')
+    print('For process '+ID)
     arrival = int(input('Enter arrival time '))
     burst = int(input('Enter CPU burst time '))
-    #completed=0
-    # waiting time
+
+    #store process data
     proc[i][0]= arrival
     proc[i][1]= burst
-    #completed =proc[i][2]
-    proc[i][2]= id
+    proc[i][2]= ID
+    
+    # put data in dictionary
     gData.update({proc[i][2]:[proc[i][0],proc[i][1]]})
-#gData = {'P1': [0,10], 'P2': [1,2], 'P3': [4,4], 'P4':[5,1], 'P5': [10,3], 'P6':[21,12]}
-gQuant = 4
-gContSwitch = 0.4
+
+# calling sjf function, store return value (tt,wt,process line)
 sjfScheduling,sjfLine = prenonsjf.sjf(gData)
+
+# calling srtf function, store return value (tt,wt,process line)
 srtfScheduling,srtfLine = prenonsjf.srtf(gData)
+
+#calling printData 
 prenonsjf.printData(gData,sjfScheduling,sjfLine,'Shortest Job First')
 prenonsjf.printData(gData,srtfScheduling,srtfLine,'Shortest Remaning Time First')
